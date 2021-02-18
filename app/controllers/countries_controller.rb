4aec20880f8bbf7e8aea7120a8e37475017db51d
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-02-18 12:26:46 +0100

    adding methods create/edit//update/destroy + views. Setup devise

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,10 +1,12 @@
 class CountriesController < ApplicationController
+  before_action :set_country, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index]
+
   def index
     @countries = Country.all
   end
 
   def show
-    @country = Country.find(params[:id])
   end
 
   def new
@@ -12,14 +14,31 @@ class CountriesController < ApplicationController
   end
 
   def create
+    @country = Country.create(country_params)
+    @country.save
+    redirect_to countries_path
   end
 
   def edit
   end
 
   def update
+    @country.update(country_params)
+    redirect_to countries_path
   end
 
   def destroy
+    @country.destroy
+    redirect_to countries_path
+  end
+
+  private
+
+  def country_params
+    params.require(:country).permit(:title, :description)
+  end
+
+  def set_country
+    @country = Country.find(params[:id])
   end
 end
