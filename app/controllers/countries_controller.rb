f6e37a2b1e053545064928b8271f46be09273513
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-02-18 10:48:34 +0100

    add methods : index/show/new + views

---
+++ app/controllers/countries_controller.rb
@@ -0,0 +1,25 @@
+class CountriesController < ApplicationController
+  def index
+    @countries = Country.all
+  end
+
+  def show
+    @country = Country.find(params[:id])
+  end
+
+  def new
+    @country = Country.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
