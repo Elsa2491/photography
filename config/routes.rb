989adee49684249a6de434fe322212a405720604
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-02-23 00:02:28 +0100

    regions_controller.rb methods edit/update/destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :countries do
     resources :regions, only: [:new, :create]
   end
+  resources :regions, only: [:edit, :update, :destroy]
+
   devise_for :users
   root to: 'countries#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
