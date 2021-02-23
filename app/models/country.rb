7437acd7ddada5a3879e7021f113b3ef25c87391
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-02-23 22:44:12 +0100

    nothing changed

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,6 +1,6 @@
 class Country < ApplicationRecord
   belongs_to :user
-  has_many :regions #, dependent: :destroy
+  has_many :regions, dependent: :destroy
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
 end
