40cc573dd39d147b4d39e5676cf6036e252e5600
Elsa Touma <71588855+Elsa2491@users.noreply.github.com>
2021-02-26 14:25:29 +0100

    Revert "remove ? region_policy & country_policy"

--- app/policies/country_policy.rb
+++ app/policies/country_policy.rb
@@ -4,15 +4,15 @@ class CountryPolicy < ApplicationPolicy
   end
 
   def create?
-    true || user.admin
+    true
   end
 
   def update?
-    record.user == user || user.admin
+    record.user == user || user.admin?
   end
 
   def destroy?
-    record.user == user || user.admin
+    record.user == user || user.admin?
   end
 
   class Scope < Scope
