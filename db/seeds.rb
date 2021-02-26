ee7c367f7647673f5bd08238f2cacdce5531e9d0
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-02-26 23:03:29 +0100

    fix pundit ?

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,8 +21,6 @@ puts 'Database cleaned'
 
 
 puts "Creating users"
-# user_1 = User.create!([{nickname: "User 1"}, {email: "user1.test@test.com"}, {password: "14PORTEFOIN"}, {password_confirmation: "14PORTEFOIN"}, {admin: false}])
-# admin = User.create!([{nickname: "Admin"}, {email: "useradmin.test@test.com"}, {password: "14PORTEFOIN"}, {password_confirmation: "14PORTEFOIN"}, {admin: true}])
 user_1 = User.create!(nickname: "User 1", email: "user1.test@test.com", password: "14PORTEFOIN", password_confirmation: "14PORTEFOIN", admin: false)
 admin_1 = User.create!(nickname: "Admin", email: "useradmin.test@test.com", password: "14PORTEFOIN", password_confirmation: "14PORTEFOIN", admin: true)
 
