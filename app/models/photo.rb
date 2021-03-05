8f788032c2f1455cbf1eb1fa46644750ff17e1bc
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-03-05 15:30:02 +0100

    add cloudinary : modified strong_params controllers, added images dans les vues, _form pour new & edit de country, option cloudinary dans config/storage.yml & chgmt dans dev.rb => cloudinary au lieu de local

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,7 @@
 class Photo < ApplicationRecord
   belongs_to :region
+  has_one_attached :image
   validates :title, presence: true
   validates :description, presence: true
+  validates :image, presence: true
 end
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,6 +1,8 @@
 class Region < ApplicationRecord
   belongs_to :country
   has_many :photos, dependent: :destroy
+  has_one_attached :image
   validates :title, presence: true
   validates :description, presence: true
+  validates :image, presence: true
 end
