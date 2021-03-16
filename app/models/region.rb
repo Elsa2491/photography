42a32078c52f9d5797bd9952274e94a89a938065
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-03-16 18:49:32 +0100

    validate uniqueness on title region

--- app/models/region.rb
+++ app/models/region.rb
@@ -2,7 +2,7 @@ class Region < ApplicationRecord
   belongs_to :country
   has_many :photos, dependent: :destroy
   has_one_attached :image
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :description, presence: true
   validates :image, presence: true
 end
