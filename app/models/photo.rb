824039312e4b2c608a3e989787f440dc57cabf71
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-03-20 13:42:06 +0100

    change image banner / remove presence true in validation description photo.rb, change links footer

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,6 @@ class Photo < ApplicationRecord
   belongs_to :region
   has_one_attached :image
   validates :title, presence: true
-  validates :description, presence: true
+  validates :description
   validates :image, presence: true
 end
