02720064ee646ba7bbb4a847611493c7c2b9f40f
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-03-21 19:52:22 +0100

    remove radius, remove validation description > photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,5 @@ class Photo < ApplicationRecord
   belongs_to :region
   has_one_attached :image
   validates :title, presence: true
-  validates :description, presence: true
   validates :image, presence: true
 end
