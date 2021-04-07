1ed1f2bf90a20e21cde6c6d573f0093824a65a36
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-04-07 15:23:37 +0200

    production: commented l.90 to 103

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,20 +87,20 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://faguoren.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = { host: 'https://faguoren.herokuapp.com' }
+  # config.action_mailer.delivery_method = :smtp
 
-  ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+  # ActionMailer::Base.smtp_settings = {
+  #   :address => 'smtp.sendgrid.net',
+  #   :port => '587',
+  #   :authentication => :plain,
+  #   :user_name => ENV['SENDGRID_USERNAME'],
+  #   :password => ENV['SENDGRID_PASSWORD'],
+  #   :domain => 'heroku.com',
+  #   :enable_starttls_auto => true
+  # }
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
